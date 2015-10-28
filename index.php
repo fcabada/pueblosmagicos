5dd1d107801713e24bd2f09f74ada29b919b44cd
Antonioquid <tiger.quid@gmail.com>
2015-10-27 21:19:49 -0600

    contador

---
+++ index.php
@@ -0,0 +1,327 @@
+<!doctype html>
+<html class="no-js">
+
+	<head>
+		<meta charset="utf-8"/>
+		<title>PUEBLOS MÁGICOS ZACATECAS</title>
+		 
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link rel="stylesheet" media="all" href="css/style.css"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<!-- Adding "maximum-scale=1" fixes the Mobile Safari auto-zoom bug: http://filamentgroup.com/examples/iosScaleBug/ -->		
+				
+		<!-- JS -->
+		<script src="js/jquery-1.7.1.min.js"></script>
+		<script src="js/custom.js"></script>
+		<script src="js/tabs.js"></script>
+		<script src="js/css3-mediaqueries.js"></script>
+		<script src="js/jquery.columnizer.min.js"></script>
+		
+		<!-- Isotope -->
+		<script src="js/jquery.isotope.min.js"></script>
+		
+		<!-- Lof slider -->
+		<script src="js/jquery.easing.js"></script>
+		<script src="js/lof-slider.js"></script>
+		<link rel="stylesheet" href="css/lof-slider.css" media="all"  /> 
+		<!-- ENDS slider -->
+		
+		<!-- Tweet -->
+		<link rel="stylesheet" href="css/jquery.tweet.css" media="all"  /> 
+		<script src="js/tweet/jquery.tweet.js" ></script> 
+		<!-- ENDS Tweet -->
+		
+		<!-- superfish -->
+		<link rel="stylesheet" media="screen" href="css/superfish.css" /> 
+		<script  src="js/superfish-1.4.8/js/hoverIntent.js"></script>
+		<script  src="js/superfish-1.4.8/js/superfish.js"></script>
+		<script  src="js/superfish-1.4.8/js/supersubs.js"></script>
+		<!-- ENDS superfish -->
+		
+		<!-- prettyPhoto -->
+		<script  src="js/prettyPhoto/js/jquery.prettyPhoto.js"></script>
+		<link rel="stylesheet" href="js/prettyPhoto/css/prettyPhoto.css"  media="screen" />
+		<!-- ENDS prettyPhoto -->
+		
+		<!-- poshytip -->
+		<link rel="stylesheet" href="js/poshytip-1.1/src/tip-twitter/tip-twitter.css"  />
+		<link rel="stylesheet" href="js/poshytip-1.1/src/tip-yellowsimple/tip-yellowsimple.css"  />
+		<script  src="js/poshytip-1.1/src/jquery.poshytip.min.js"></script>
+		<!-- ENDS poshytip -->
+		
+		<!-- JCarousel -->
+		<script type="text/javascript" src="js/jquery.jcarousel.min.js"></script>
+		<link rel="stylesheet" media="screen" href="css/carousel.css" /> 
+		<!-- ENDS JCarousel -->
+		
+		<!-- GOOGLE FONTS -->
+		<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
+
+		<!-- modernizr -->
+		<script src="js/modernizr.js"></script>
+		
+		<!-- SKIN -->
+		<link rel="stylesheet" media="all" href="css/skin.css"/>
+		
+		<!-- Less framework -->
+		<link rel="stylesheet" media="all" href="css/lessframework.css"/>
+		
+		<!-- flexslider -->
+		<link rel="stylesheet" href="css/flexslider.css" >
+		<script src="js/jquery.flexslider.js"></script>
+
+	</head>
+	
+	
+	<body class="home">
+	
+		<!-- HEADER -->
+		<header>
+			<div class="wrapper cf">
+				
+				<div id="logo">
+					<blockquote>
+					  <blockquote>
+					    
+				      </blockquote>
+				  </blockquote>
+				</div>
+				
+				<!-- nav -->
+				<ul id="nav" class="sf-menu">
+					<li class="current-menu-item"><a href="index.html">INICIO</a></li>
+					<li><a href="pueblos.html">PUEBLOS MÁGICOS</a></li>
+					<li><a href="page.html">UBICACION</a></li>
+					<li><a href="contact.html">CONTACTO</a></li>
+				</ul>
+				<div id="combo-holder"></div>
+				<!-- ends nav -->
+				
+				
+				<!-- SLIDER -->				
+				<div id="home-slider" class="lof-slidecontent">
+					
+					<div class="preload"><div></div></div>
+					
+					<!-- slider content --> 
+					<div class="main-slider-content" >
+					<ul class="sliders-wrap-inner">
+					    <li>
+					          <img src="img/dummies/slides/01.jpg" title="" alt="alt" />           
+					          <div class="slider-description">
+					            <h4>JEREZ DE GARCÍA SALINAS</h4>
+					            <p>Esta apacible población donde se saborea el ambiente provinciano, es conocida por ser la cuna del poeta Ramón López Velarde, y se convirtió en el primer "Pueblo Mágico" de Zacatecas. Con este estatus, desde el año 2007 Jerez se promueve como destino turístico nacional e internacional. 
+					            <a class="link" href="#">Read more </a>
+					            </p>
+					         </div>
+					    </li>
+					    
+					    <li>
+					          <img src="img/dummies/slides/02.jpg" title="" alt="alt" />           
+					          <div class="slider-description">
+					            <h4>SOMBRERETE</h4>
+					            <p>Esta población, cuyo nombre deriva del cercano y dominante cerro del “Sombreretillo, por la forma que éste tiene de sombrero tricornio de la época, fue fundado en 1555 por Juan de Tolosa y en sus inicios fue nombrada como Villa de Llerena de San Juan Bautista y Real de Minas de Sombrerete, con una fama de potenciales e inagotable yacimientos de oro y plata explotados desde esa época. 
+					            <a class="link" href="#">Read more </a>
+					            </p>
+					         </div>
+					    </li>
+					    
+					    <li>
+					          <img src="img/dummies/slides/03.jpg" title="" alt="alt" />           
+					          <div class="slider-description">
+					            <h4>NOCHISTLÁN DE MEJÍA</h4>
+					            <p>Ninguno como éste para conjuntar el pentagrama de los cinco baluartes turísticos que circundan el estado de Zacatecas. Y es que de ninguna manera su belleza e importancia histórica podían ser indiferentes, ni quedar fuera de este justo reconocimiento como Pueblo Mágico. Con este su nombramiento, en noviembre de 2012, Nochistlán recupera su nivel e importancia histórica de antaño. Esta población, cuyo nombre puede ser entendido como "lugar de tunas" o "lugar de grana", se encuentra situada al sur de la entidad, a una distancia de 220 km. de la capital estatal (2 ½ hrs.). 
+					            <a class="link" href="#">Read more </a>
+					            </p>
+					         </div>
+					    </li>
+					    
+					    <li>
+					          <img src="img/dummies/slides/04.jpg" title="" alt="alt" />           
+					          <div class="slider-description">
+					            <h4>TEÚL DE GONZÁLEZ ORTEGA</h4>
+					            <p>Teúl (teotl) Dios, es también un adjetivo que califica a los chichimecas como sagrados, venerados, respetables (Tetl) "Gran piedra redonda", según otros. 
+"De González Ortega", porque a partir de 1800 llega al Teúl un tío del Gral. J. Jesús González Ortega el cual, dadas sus cualidades docentes, funda una escuela. Sin precisar fecha, pero muy probablemente a la edad de cinco años, llega también a vivir al Teúl el niño Jesús González Ortega; después de sus estudios en el Seminario de Guadalajara aparece de nuevo en el Teúl, como escribiente en el juzgado. Por esta razón, durante mucho tiempo se creyó que era originario del Teúl.
+					            <a class="link" href="#">Read more </a>
+					            </p>
+					         </div>
+					    </li>
+					    
+					    <li>
+					          <img src="img/dummies/slides/05.jpg" title="" alt="alt" />           
+					          <div class="slider-description">
+					            <h4>PINOS</h4>
+					            <p>Este Real de Nuestra Señora de Sierra de Pinos, fue fundado en la segunda mitad del s. XVI, durante las exploraciones españolas realizadas por el capitán mestizo Zacatecano Miguel Caldera. 
+								Como toda población minera, la irregular topografía natural propició el forjamiento de un conglomerado pero encantador conjunto de viviendas que con el transcurrir dieron forma e importancia al real, surgiendo estupendos edificios religiosos como la parroquia de San Matías, el templo de Tlaxcalita, el de San Francisco y el de la Santa Veracruz.
+					            <a class="link" href="#">Read more </a>
+					            </p>
+					         </div>
+					    </li>
+					    
+					   
+					    
+					  </ul>  	
+					</div>
+					<!-- ENDS slider content --> 
+				           
+					<!-- slider nav -->
+					<div class="navigator-content">
+					  <div class="navigator-wrapper">
+					        <ul class="navigator-wrap-inner">
+					           <li><img src="img/dummies/slides/01_thumb.jpg" alt="alt" /></li>
+					           <li><img src="img/dummies/slides/02_thumb.jpg" alt="alt" /></li>
+					           <li><img src="img/dummies/slides/03_thumb.jpg" alt="alt" /></li>
+					           <li><img src="img/dummies/slides/04_thumb.jpg" alt="alt" /></li>
+					           <li><img src="img/dummies/slides/05_thumb.jpg" alt="alt" /></li>
+					           <li><img src="img/dummies/slides/06_thumb.jpg" alt="alt" /></li>
+					           <li><img src="img/dummies/slides/07_thumb.jpg" alt="alt" /></li>
+					        </ul>
+					  </div>
+					  <div class="button-next">Next</div>
+					  <div  class="button-previous">Previous</div>
+					  <!-- REMOVED TILL FIXED <div class="button-control"><span>STOP</span></div> -->
+					</div> 
+					<!-- slider nav -->
+					
+					
+				          
+			  </div> 
+				<!-- ENDS SLIDER -->
+
+
+
+
+
+			</div>
+		</header>
+		<!-- ENDS HEADER -->
+		
+		
+		<!-- MAIN -->
+		<div id="main">
+			<div class="wrapper cf">
+							
+			<div>
+			  <table width="936" border="1">
+                <tr>
+                  <td width="300" bordercolor="#FFFFFF">
+				 
+				  <audio src="lanegra.mp3" autoplay controls> </audio></td>
+                  <td width="300" bordercolor="#FFFFFF">&nbsp;</td>
+                 <!-- <td width="300" bordercolor="#FFFFFF"><div align="right">Visitante No. 456</div></td> -->
+                 <?php
+//archivo de visitas
+$archivo = "numero.dat";
+
+//Abrimos elarchivo solo lectura
+$abre = fopen($archivo,"r");
+
+//obtenemoselvaloractualdevisitas
+$total = fread($abre,filesize($archivo));
+
+//Cerramos la conexiónal archivo
+fclose($abre);
+
+//Abrimosnuevamenteelarchivo
+$abre = fopen($archivo,"w");
+//Sumamos1 nueva visita
+$total = $total+1;
+//Y reemplazamos por la nueva cantidad
+$grabar = fwrite ($abre,$total);
+//Cerramoslaconexiónalarchivo
+fclose($abre);
+//Imprimimostotaldevisitas
+echo"<td width='400'> <fontface='Kristen ITC'size='3'><fontcolor='#FFFFFF'><div align='right'>Visitas:<fontcolor='#FFFFFF'>".$total."</font></td>";
+?> 
+
+               
+               
+                </tr>
+              </table>
+			</div>
+			<!-- featured -->
+			<div class="home-featured">
+			
+				<ul id="filter-buttons">
+					<li><a href="#" data-filter="*" class="selected">NUESTROS PUEBLOS MÁGICOS</a></li>
+					<li><a href="#" data-filter=".web">GALERIA</a></li>
+					<li><a href="#" data-filter=".print">VIDEOS</a></li>
+					
+				</ul>
+				
+				<!-- Filter container -->
+				<div id="filter-container" class="cf">
+					<figure class="web">
+						<a href="project.html" class="thumb"><img src="img/dummies/featured/01.jpg" alt="alt" /></a>
+						<figcaption>
+							<a href="project.html"><h3 class="heading">Jerez </h3></a>
+							Se dice que el nombre de Jerez proviene de un vocablo árabe que significa "lugar donde abundan los pastizales", y que sus fundadores fueron andaluces extremeños, pero lo cierto es que su documentación histórica reseña que su fundación se lleva a cabo en la segunda mitad del siglo XVI.  </figcaption>
+					</figure>
+					
+					<figure class="print">
+						<a href="project.html" class="thumb"><img src="img/dummies/featured/02.jpg" alt="alt" /></a>
+						<figcaption>
+							<a href="project.html"><h3 class="heading">Sombrerete</h3></a>
+							A poco más de 170 km al norte de la capital zacatecana se localiza una de las poblaciones coloniales más sobresalientes del norte de México, declarado a partir de febrero de 2012 “Pueblo Mágico” de México y el tercero de la lista del estado de Zacatecas.  </figcaption>
+					</figure>
+					
+					
+					<figure class="design">
+						<a href="project.html" class="thumb"><img src="img/dummies/featured/03.jpg" alt="alt" /></a>
+						<figcaption>
+							<a href="project.html" ><h3 class="heading">Teúl de González Ortega</h3></a>
+							Se conoce poco sobre sus primeros pobladores, pero se cree que las primeras ocupaciones ocurrieron fueron en el primer siglo de nuestra era. Esto se deduce por la aparición de tumbas de tiro, que datan de esta época. Los caxcanes llegan hacia la mitad del siglo XII, y arrojan a sus parientes nahoas los tecuexes de los valles de Tlaltenango y el Teúl. </figcaption>
+					</figure>
+					
+					
+					<figure class="photo">
+						<a href="project.html" class="thumb"><img src="img/dummies/featured/04.jpg" alt="alt" /></a>
+						<figcaption>
+							<a href="project.html" ><h3 class="heading">Pinos</h3></a>
+							Pinos cuenta con singulares tradiciones, como su feria regional de San Matías y la Fiesta de los Faroles, que se celebra el 8 de diciembre. 
+
+Así también, sus sólidas raíces afloran en su folklore artesanal, musical y gastronómico, realizando magníficos textiles, coloquios populares, danzas de Matlachines y de Pluma y una moderada destilación de exquisitos mezcales. Por lo anterior y por más, en octubre del 2012 fue reconocido y declarado “Pueblo Mágico”, siendo con ello, el cuarto de la lista de localidades de Zacatecas con esta distinción. </figcaption>
+					</figure>
+					
+					
+					<figure class="web photo">
+						<a href="project.html" class="thumb"><img src="img/dummies/featured/05.jpg" alt="alt" /></a>
+						<figcaption>
+							<a href="project.html" ><h3 class="heading">Nochistlán</h3></a>
+							Es conocido con el nombre oficial de Nochistlán de Mejía, en honor al Coronel Jesús Mejia, quien fue defensor de la plaza en la invasión francesa, y durante la época prehispánica estuvo habitado por los indígenas caxcanes, quienes lucharon bravíamente en contra de la inevitable conquista española.  </figcaption>
+					</figure>									
+				</div><!-- ENDS Filter container -->
+				
+			</div>
+			<!-- ENDS featured -->
+									
+			</div><!-- ENDS WRAPPER -->
+		</div>
+		<!-- ENDS MAIN -->
+		
+		
+		<!-- FOOTER -->
+		<footer>
+			<div class="wrapper cf">
+              <!-- widgets -->
+              <!-- ENDS widgets -->
+              <!-- bottom -->
+  <div class="footer-bottom">
+    <ul id="social-bar" class="cf sb">
+						    <li><a href="http://www.facebook.com"  title="Become a fan" class="facebook">Facebbok</a></li>
+							  <li><a href="http://www.twitter.com" title="Follow my tweets" class="twitter"></a></li>
+							  <li><a href="http://plus.google.com" title="Enter my circles" class="plus"></a></li>
+						  </ul>
+			  </div>	
+				  <!-- ENDS bottom -->
+			  
+		  </div></footer>
+		<!-- ENDS FOOTER -->
+		
+	</body>
+	
+	
+	
+</html>
